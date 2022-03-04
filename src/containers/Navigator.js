97d0929505e355e113380b7a1bf91b2a121e8b83
Vipula <vipula.jivani@solutionanalysts.com>
2022-03-04 17:59:31 +0530

    solve issue of hiding bottombar in detail screen by defining proper route

--- src/containers/Navigator.js
+++ src/containers/Navigator.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import BottomTabLayout from './BottomTabLayout';
 import {SplashScreen} from './SplashScreen';
-
+import {createStackNavigator} from '@react-navigation/stack';
+import EventDetailScreen from './EventDetailScreen';
+import {NavigationContainer} from '@react-navigation/native';
 const Navigator = () => {
   const [visible, setVisible] = useState(false);
 
@@ -9,7 +11,31 @@ const Navigator = () => {
     setVisible(true);
   }, 3000);
 
-  return !visible ? <SplashScreen /> : <BottomTabLayout />;
+  const Stack = createStackNavigator();
+
+  const HomeStack = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen
+            name="Home"
+            component={BottomTabLayout}
+            options={{title: 'Home Page'}}
+          />
+          <Stack.Screen
+            name="Details"
+            component={EventDetailScreen}
+            options={{title: 'Details Page'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
+  return !visible ? <SplashScreen /> : <HomeStack />;
 };
 
 export default Navigator;
